adda5781777e2eebda62dbf5a88eea33f1f8a0e2
Sebastian Trzuskot <w57006@student.wsiz.rzeszow.pl>
2018-04-12 18:40:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/stud/recyclerview/Planeta.java
@@ -0,0 +1,14 @@
+package com.example.stud.recyclerview;
+
+
+
+public class Planeta {
+    String nazwa;
+    String rodzaj;
+    int ksiezyce;
+    public Planeta(String nazwa, String rodzaj, int ksiezyce) {
+        this.nazwa = nazwa;
+        this.rodzaj = rodzaj;
+        this.ksiezyce = ksiezyce;
+    }
+}
